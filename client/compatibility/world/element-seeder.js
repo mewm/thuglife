2eee84a59499c804929559a094c99938a0f1da48
SeverinDK <some12thing@gmail.com>
2014-12-19 15:44:18 +0100

    Changed settings for DenniZ preview

--- client/compatibility/world/element-seeder.js
+++ client/compatibility/world/element-seeder.js
@@ -5,7 +5,7 @@ var elementSeeder = (function()
 		{
 			var shroomBatch = new PIXI.SpriteBatch();
 			this.stage.addChild(shroomBatch);
-			var shroomsNo = 100;
+			var shroomsNo = 20;
 			for (var i = 0; i < shroomsNo; i++) {
 				// Create a new critter in the center of the map with some random offset.
 				var element = elementFactory.createShroom(Math.random() * 500, Math.random() * 500);
@@ -18,7 +18,7 @@ var elementSeeder = (function()
 
 
 
-			var bunnies = 5;
+			var bunnies = 3;
 			for (var i = 0; i < bunnies; i++) {
 				// Create a new critter in the center of the map with some random offset.
 				var element = elementFactory.createBunny(Math.random() * 500, Math.random() * 500);
